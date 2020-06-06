72d0c817db4bede7884eb3b995d715ec6d449bd6
Scipi <rolandk@fokus.hu>
2020-06-06 23:39:17 +0200

    Fix run.bat for windows

--- src/coin_collect/run.bat
+++ src/coin_collect/run.bat
@@ -1,3 +1,3 @@
 set script_dir=%~dp0
-VENV_PYTHON_INTERPRETER=..\..\venv\Scripts/python.exe
-%VENV_PYTHON_INTERPRETER% coin_collect.py
+set VENV_PYTHON_INTERPRETER=..\..\venv\Scripts\python.exe
+start /min %VENV_PYTHON_INTERPRETER% coin_collect.py
